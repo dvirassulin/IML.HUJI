431350664f62b38a670eab9bc871c00438d9ae20
Dvir Assulin <dvir.assulin@gmail.com>
2022-05-31 16:32:19 +0300

    Done with q1-q5

--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -27,13 +27,51 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
     """
     # Question 1 - Generate dataset for model f(x)=(x+3)(x+2)(x+1)(x-1)(x-2) + eps for eps Gaussian noise
     # and split into training- and testing portions
-    raise NotImplementedError()
+    response = lambda x:  (x + 3) * (x + 2) * (x + 1) * (x - 1) * (x - 2)
+
+    X = np.linspace(-1.2, 2, n_samples)
+    y_ = response(X)
+    y = y_ + np.random.normal(scale=noise, size=len(y_))
+
+    train_x, train_y, test_x, test_y = split_train_test(pd.DataFrame(X), y, 2/3)
+    train_x, train_y, test_x, test_y = train_x.to_numpy().flatten(), train_y.to_numpy().flatten(), \
+                                       test_x.to_numpy().flatten(), test_y.to_numpy().flatten()
+
+
+    fig = go.Figure(data=[
+            go.Scatter(x=X, y=y_, mode="markers+lines", name="Real Points",  marker=dict(color="black", opacity=.7)),
+            go.Scatter(x=train_x, y=train_y, mode="markers", name="Predicted train",  marker=dict(color="red", opacity=.7)),
+            go.Scatter(x=test_x, y=test_y, mode="markers", name="Predicted test",  marker=dict(color="blue", opacity=.7))],
+        layout=go.Layout(title_text=rf"$\text{{Polynomial Fitting of Degree 5 - Sample Noise }}\mathcal{{N}}\left(0,{noise}\right)$",
+                         xaxis={"title": r"$x$"},
+                         yaxis={"title": r"$y$"}))
+    fig.show()
+
 
     # Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
-    raise NotImplementedError()
+    degrees = 10
+    train_errors = np.zeros(degrees + 1)
+    validate_errors = np.zeros(degrees + 1)
+    for k in range(degrees + 1):
+        train_errors[k], validate_errors[k] = cross_validate(PolynomialFitting(k), train_x, train_y, mean_square_error, 5)
+
+    x_axis = [k for k in range(degrees + 1)]
+    fig1 = go.Figure(data=[
+            go.Scatter(x=x_axis, y=train_errors, mode="markers", name="Train error",  marker=dict(color="red", opacity=.7)),
+            go.Scatter(x=x_axis, y=validate_errors, mode="markers", name="Validate error",  marker=dict(color="blue", opacity=.7))],
+        layout=go.Layout(title_text=rf"$\text{{Errors with cross-validation as function of polynomial degree}}$",
+                         xaxis={"title": r"$Degee$"},
+                         yaxis={"title": r"$Error$"}))
+    fig1.show()
+
 
     # Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
-    raise NotImplementedError()
+
+    min_validate_error_degree = np.argmin(validate_errors)
+    poly_fit_model = PolynomialFitting(min_validate_error_degree).fit(train_x, train_y)
+    test_error = mean_square_error(test_y, poly_fit_model.predict(test_x))
+    print(f"The minimal test error is {round(test_error, 2)}, and achieved with polinomial"
+          f" degree of {min_validate_error_degree}")
 
 
 def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 500):
@@ -61,4 +99,7 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
 
 if __name__ == '__main__':
     np.random.seed(0)
-    raise NotImplementedError()
+    n_samples, noises = [100, 100, 1500], [5, 0, 10]
+    for sample_size, noise in zip(n_samples, noises):
+        select_polynomial_degree(sample_size, noise)
+
